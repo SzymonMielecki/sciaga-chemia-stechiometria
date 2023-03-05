6e837fe5a8c5ea2c88fc7c67d50b769a5a814fb5
Mac <mac@MacBook-Szymon.local>
2023-03-05 14:22:52 +0100

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,20 @@
+<script>
+	import '@skeletonlabs/skeleton/themes/theme-hamlindigo.css';
+	import '@skeletonlabs/skeleton/styles/all.css';
+	import '../app.postcss';
+	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
+</script>
+
+<!-- App Shell -->
+<AppShell>
+	<AppBar>
+		<h1 class="flex justify-center">
+			<span
+				class="bg-gradient-to-br from-primary-500 via-tertiary-500 to-secondary-500 bg-clip-text text-transparent box-decoration-clone"
+			>
+				Stechiometria - sprawdzian
+			</span>
+		</h1>
+	</AppBar>
+	<slot />
+</AppShell>
